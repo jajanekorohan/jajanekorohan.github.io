0cb5127c17d8ae72f40545cf96554a1590147244
jajanekorohan <129662743+jajanekorohan@users.noreply.github.com>
2023-04-06 18:51:04 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(function() {
         let speed = 500;
         let href= $(this).attr("href");
         let target = $(href == "#" || href == "" ? 'html' : href);
-        let position = target.offset().top;
+        let position = target.offset().top - 50; // ヘッダーの高さ分だけスクロール位置をずらす
         $("html, body").animate({scrollTop:position}, speed, "swing");
         return false;
     });
